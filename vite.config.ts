16bf5bbe79a2ca3ed0d553c8c68802f3abfecaa2
priyanka-sharma181 <priyanka.sharma@madgicaltechdom.com>
2025-08-06 19:45:26 +0530

    Configure GitHub Pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,8 @@
+// vite.config.ts
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
+  base: '/Cover-page/', // 👈 important
   plugins: [react()],
-  optimizeDeps: {
-    exclude: ['lucide-react'],
-  },
 });
